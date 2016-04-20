ba739574a24d2669197f3a808e2c4c23c4f7d116
helgi <helgith@gmail.com>
2016-04-20 13:41:50 -0700

    feat(registry): add initial support to auth to an external Registry on per app basis
    This will allow for setting username and password for an external registry to pull images from
    
    IMAGE_AUTH_USER and IMAGE_AUTH_PASSWORD are support via config:set and this is per application.
    
    Currently Docker Hub and Quay are supported and a basic support for long lived tokens under gcr.io - ECR is not supported at this time.
    
    For GCR see https://cloud.google.com/container-registry/docs/auth#using_a_json_key_file

--- rootfs/registry/tests.py
+++ rootfs/registry/tests.py
@@ -5,13 +5,11 @@ Run the tests with "./manage.py test registry"
 """
 
 import unittest
-try:
-    from unittest import mock
-except ImportError:
-    import mock
+from unittest import mock
 
 from django.conf import settings
 from rest_framework.exceptions import PermissionDenied
+from registry import publish_release, RegistryException
 from registry.dockerclient import DockerClient
 
 
@@ -24,23 +22,130 @@ class DockerClientTest(unittest.TestCase):
 
     def test_publish_release(self, mock_client):
         self.client = DockerClient()
-        self.client.publish_release('ozzy/embryo:git-f2a8020', 'ozzy/embryo:v4', True)
+
+        # Make sure login is not called when there are no creds
+        publish_release('ozzy/embryo:git-f2a8020', 'ozzy/embryo:v4', False)
+        self.assertFalse(self.client.client.login.called)
+
+        creds = {
+            'username': 'fake',
+            'password': 'fake',
+            'email': 'fake',
+            'registry': 'quay.io'
+        }
+
+        client = {}
+        client['Status'] = 'Login Succeeded'
+        self.client.client.login.return_value = client
+        publish_release('ozzy/embryo:git-f2a8020', 'ozzy/embryo:v4', False, creds)
+        self.assertTrue(self.client.client.login.called)
         self.assertTrue(self.client.client.pull.called)
         self.assertTrue(self.client.client.tag.called)
         self.assertTrue(self.client.client.push.called)
+
+        publish_release('ozzy/embryo:git-f2a8020', 'ozzy/embryo:v4', True)
+        self.assertTrue(self.client.client.pull.called)
+        self.assertTrue(self.client.client.tag.called)
+        self.assertTrue(self.client.client.push.called)
+
         # Test that a registry host prefix is replaced with deis-registry for the target
-        self.client.publish_release('ozzy/embryo:git-f2a8020', 'quay.io/ozzy/embryo:v4', True)
+        publish_release('ozzy/embryo:git-f2a8020', 'quay.io/ozzy/embryo:v4', True)
         docker_push = self.client.client.push
         docker_push.assert_called_with(
             'localhost:5000/ozzy/embryo', tag='v4', insecure_registry=True,
             decode=True, stream=True)
+
         # Test that blacklisted image names can't be published
         with self.assertRaises(PermissionDenied):
-            self.client.publish_release(
+            publish_release(
                 'deis/controller:v1.11.1', 'deis/controller:v1.11.1', True)
         with self.assertRaises(PermissionDenied):
-            self.client.publish_release(
+            publish_release(
                 'localhost:5000/deis/controller:v1.11.1', 'deis/controller:v1.11.1', True)
+
+    def test_login(self, mock_client):
+        self.client = DockerClient()
+
+        # success
+        client = {}
+        client['Status'] = 'Login Succeeded'
+        self.client.client.login.return_value = client
+
+        creds = {
+            'username': 'fake',
+            'password': 'fake',
+            'email': 'fake',
+            'registry': 'quay.io'
+        }
+        self.client.login('quay.io/deis/foobar', creds)
+        docker_login = self.client.client.login
+        docker_login.assert_called_with(
+            username='fake', password='fake',
+            email='fake', registry='quay.io'
+        )
+
+        # username matches
+        client = {}
+        client['username'] = 'fake'
+        self.client.client.login.return_value = client
+
+        creds = {
+            'username': 'fake',
+            'password': 'fake',
+            'email': 'fake',
+            'registry': 'quay.io'
+        }
+        self.client.login('quay.io/deis/foobar', creds)
+        docker_login = self.client.client.login
+        docker_login.assert_called_with(
+            username='fake', password='fake',
+            email='fake', registry='quay.io'
+        )
+
+    def test_login_failed(self, mock_client):
+        self.client = DockerClient()
+
+        # failed login
+        client = {}
+        client['Status'] = 'Login Failed'
+        self.client.client.login.return_value = client
+
+        creds = {
+            'username': 'fake',
+            'password': 'fake',
+            'email': 'fake',
+            'registry': 'quay.io'
+        }
+
+        with self.assertRaises(PermissionDenied):
+            self.client.login('quay.io/deis/foobar', creds)
+            docker_login = self.client.client.login
+            docker_login.assert_called_with(
+                username='fake', password='fake',
+                email='fake', registry='quay.io'
+            )
+
+    def test_login_bad_creds(self, mock_client):
+        self.client = DockerClient()
+
+        # missing parts of credentials
+        with self.assertRaises(PermissionDenied):
+            creds = {
+                'username': 'fake',
+                'email': 'fake',
+                'registry': 'quay.io'
+            }
+            self.client.login('quay.io/deis/foobar', creds)
+
+        # bad credentials
+        with self.assertRaises(PermissionDenied):
+            creds = {
+                'username': 'fake',
+                'password': 'fake',
+                'email': 'fake',
+                'registry': 'quay.io'
+            }
+            self.client.login('quay.io/deis/foobar', creds)
 
     def test_pull(self, mock_client):
         self.client = DockerClient()
@@ -67,8 +172,15 @@ class DockerClientTest(unittest.TestCase):
         docker_tag = self.client.client.tag
         docker_tag.assert_called_once_with(
             'ozzy/embryo:git-f2a8020', 'ozzy/embryo', tag='v4', force=True)
+
+        # fake failed tag
+        self.client.client.tag.return_value = False
+        with self.assertRaises(RegistryException):
+            self.client.tag('foo/bar:latest', 'foo/bar', 'v1.11.1')
+
         # Test that blacklisted image names can't be tagged
         with self.assertRaises(PermissionDenied):
             self.client.tag('deis/controller:v1.11.1', 'deis/controller', 'v1.11.1')
+
         with self.assertRaises(PermissionDenied):
             self.client.tag('localhost:5000/deis/controller:v1.11.1', 'deis/controller', 'v1.11.1')

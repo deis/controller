0f7a51eda596c440aa08cf3281ce91ff669b6aa6
Keerthan Mala <kmala@engineyard.com>
2016-06-20 16:37:36 -0600

    remove unnecessary set health check function

--- rootfs/api/models/config.py
+++ rootfs/api/models/config.py
@@ -54,35 +54,6 @@ class Config(UuidAuditedModel):
             'failure_threshold': failure_threshold,
         }
 
-    def set_env_healthchecks(self):
-        """Defines default values for HTTP healthchecks"""
-        if not {k: v for k, v in self.values.items() if k.startswith('HEALTHCHECK_')}:
-            return
-
-        # fetch set health values and any defaults
-        # this approach allows new health items to be added without issues
-        health = self.env_healthcheck()
-        if not health:
-            return
-
-        # HTTP GET related
-        self.values['HEALTHCHECK_URL'] = health['path']
-
-        # Number of seconds after which the probe times out.
-        # More info: http://releases.k8s.io/HEAD/docs/user-guide/pod-states.md#container-probes
-        self.values['HEALTHCHECK_TIMEOUT'] = health['timeout']
-        # Number of seconds after the container has started before liveness probes are initiated.
-        # More info: http://releases.k8s.io/HEAD/docs/user-guide/pod-states.md#container-probes
-        self.values['HEALTHCHECK_INITIAL_DELAY'] = health['delay']
-        # How often (in seconds) to perform the probe.
-        self.values['HEALTHCHECK_PERIOD_SECONDS'] = health['period_seconds']
-        # Minimum consecutive successes for the probe to be considered successful
-        # after having failed.
-        self.values['HEALTHCHECK_SUCCESS_THRESHOLD'] = health['success_threshold']
-        # Minimum consecutive failures for the probe to be considered failed after
-        # having succeeded.
-        self.values['HEALTHCHECK_FAILURE_THRESHOLD'] = health['failure_threshold']
-
     def set_registry(self):
         # lower case all registry options for consistency
         self.registry = {key.lower(): value for key, value in self.registry.copy().items()}
@@ -144,7 +115,6 @@ class Config(UuidAuditedModel):
                         data[key] = value
                 setattr(self, attr, data)
 
-            self.set_env_healthchecks()
             self.set_registry()
             self.set_tags(previous_config)
         except Config.DoesNotExist:

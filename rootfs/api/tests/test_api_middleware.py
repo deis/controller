498ff2d529215652781e3946459df779cde7d2aa
Joshua Anderson <joshua@deis.com>
2016-08-01 15:40:06 -0700

    fix(middleware): remove unused server side version check

--- rootfs/api/tests/test_api_middleware.py
+++ rootfs/api/tests/test_api_middleware.py
@@ -24,29 +24,9 @@ class APIMiddlewareTest(APITestCase):
 
     def test_deis_version_header_good(self):
         """
-        Test that when the version header is sent, the request is accepted.
-        """
-        response = self.client.get(
-            '/v2/apps',
-            HTTP_DEIS_VERSION=__version__.rsplit('.', 2)[0]
-        )
-        self.assertEqual(response.status_code, 200, response.data)
-        self.assertEqual(response.has_header('DEIS_API_VERSION'), True)
-        self.assertEqual(response['DEIS_API_VERSION'], __version__.rsplit('.', 1)[0])
-
-    def test_deis_version_header_bad(self):
-        """
-        Test that when an improper version header is sent, the request is declined.
-        """
-        response = self.client.get(
-            '/v2/apps',
-            HTTP_DEIS_VERSION='1234.5678'
-        )
-        self.assertEqual(response.status_code, 405, response.content)
-
-    def test_deis_version_header_not_present(self):
-        """
-        Test that when the version header is not present, the request is accepted.
+        Test that when the version header is sent.
         """
         response = self.client.get('/v2/apps')
-        self.assertEqual(response.status_code, 200, response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.has_header('DEIS_API_VERSION'), True)
+        self.assertEqual(response['DEIS_API_VERSION'], __version__.rsplit('.', 1)[0])

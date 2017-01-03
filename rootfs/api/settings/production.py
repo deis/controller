27bc43864d0a4790b1e833035d666a7a1ec441ce
Matthew Fisher <mfisher@deis.com>
2017-01-03 11:04:54 -0800

    fix(settings): disable LDAP by default

--- rootfs/api/settings/production.py
+++ rootfs/api/settings/production.py
@@ -109,7 +109,6 @@ INSTALLED_APPS = (
 )
 
 AUTHENTICATION_BACKENDS = (
-    "django_auth_ldap.backend.LDAPBackend",
     "django.contrib.auth.backends.ModelBackend",
     "guardian.backends.ObjectPermissionBackend",
 )
@@ -367,31 +366,33 @@ LDAP_GROUP_FILTER = os.environ.get('LDAP_GROUP_FILTER', '')
 # verbose logging from auth-ldap plugin:
 # https://pythonhosted.org/django-auth-ldap/logging.html
 
-AUTH_LDAP_SERVER_URI = LDAP_ENDPOINT
-AUTH_LDAP_BIND_DN = LDAP_BIND_DN
-AUTH_LDAP_BIND_PASSWORD = LDAP_BIND_PASSWORD
-AUTH_LDAP_USER_SEARCH = LDAPSearch(
-    base_dn=LDAP_USER_BASEDN,
-    scope=ldap.SCOPE_SUBTREE,
-    filterstr="(%s=%%(user)s)" % LDAP_USER_FILTER
-)
-AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
-    base_dn=LDAP_GROUP_BASEDN,
-    scope=ldap.SCOPE_SUBTREE,
-    filterstr="(%s)" % LDAP_GROUP_FILTER
-)
-AUTH_LDAP_GROUP_TYPE = GroupOfNamesType()
-AUTH_LDAP_USER_ATTR_MAP = {
-    "first_name": "givenName",
-    "last_name": "sn",
-    "email": "mail",
-    "username": LDAP_USER_FILTER,
-}
-AUTH_LDAP_GLOBAL_OPTIONS = {
-    ldap.OPT_X_TLS_REQUIRE_CERT: False,
-    ldap.OPT_REFERRALS: False
-}
-AUTH_LDAP_ALWAYS_UPDATE_USER = True
-AUTH_LDAP_MIRROR_GROUPS = True
-AUTH_LDAP_FIND_GROUP_PERMS = True
-AUTH_LDAP_CACHE_GROUPS = False
+if LDAP_ENDPOINT:
+    AUTHENTICATION_BACKENDS = ("django_auth_ldap.backend.LDAPBackend",) + AUTHENTICATION_BACKENDS
+    AUTH_LDAP_SERVER_URI = LDAP_ENDPOINT
+    AUTH_LDAP_BIND_DN = LDAP_BIND_DN
+    AUTH_LDAP_BIND_PASSWORD = LDAP_BIND_PASSWORD
+    AUTH_LDAP_USER_SEARCH = LDAPSearch(
+        base_dn=LDAP_USER_BASEDN,
+        scope=ldap.SCOPE_SUBTREE,
+        filterstr="(%s=%%(user)s)" % LDAP_USER_FILTER
+    )
+    AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
+        base_dn=LDAP_GROUP_BASEDN,
+        scope=ldap.SCOPE_SUBTREE,
+        filterstr="(%s)" % LDAP_GROUP_FILTER
+    )
+    AUTH_LDAP_GROUP_TYPE = GroupOfNamesType()
+    AUTH_LDAP_USER_ATTR_MAP = {
+        "first_name": "givenName",
+        "last_name": "sn",
+        "email": "mail",
+        "username": LDAP_USER_FILTER,
+    }
+    AUTH_LDAP_GLOBAL_OPTIONS = {
+        ldap.OPT_X_TLS_REQUIRE_CERT: False,
+        ldap.OPT_REFERRALS: False
+    }
+    AUTH_LDAP_ALWAYS_UPDATE_USER = True
+    AUTH_LDAP_MIRROR_GROUPS = True
+    AUTH_LDAP_FIND_GROUP_PERMS = True
+    AUTH_LDAP_CACHE_GROUPS = False
